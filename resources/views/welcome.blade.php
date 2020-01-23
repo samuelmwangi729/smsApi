f8e45da2243085964372e52f433f146c408d5f9b
samuel mwangi <samuelmwangi729@gmail.com>
2020-01-23 04:52:15 +0300

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="row">
     @foreach ($numbers as $number)
-    <div class="col-sm-2">
+    <div class="col-sm-2 col-xs-2 col-md-2 col-lg-2">
         {{ $number->number }}
     </div>
     @endforeach
