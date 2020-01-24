5854f7ac0674ae6eab82d9ea84be9e16a6504f89
samuel mwangi <samuelmwangi729@gmail.com>
2020-01-24 09:47:46 +0300

    new

---
+++ app/Http/Controllers/PhoneController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Number;
+use Illuminate\Http\Request;
+
+class PhoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $count=Number::where('number',$request->phone)->get()->count();
+        if($count==0){
+            $data=['user'=>$request->user,'time'=>$request->time,'phone'=>$request->phone,'status'=>'Not Found','res'=>'false'];
+            return response()->json([$data],200);
+        }else{
+            $data=['user'=>$request->user,'time'=>$request->time,'phone'=>$request->phone,'status'=>'Found','res'=>'true'];
+            return response()->json([$data],200);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,9 @@ Route::get('/','GenerateController@generate');
 Route::get('/login','GenerateController@generate');
 Route::get('/register','GenerateController@generate');
 Auth::routes();
+Route::get('/Api/GetPhone/user={user}&time={time}&phone={phone}',[
+    'uses'=>'PhoneController@index',
+    'as'=>'phone.get'
+]);
 
 Route::get('/home', 'HomeController@index')->name('home');
